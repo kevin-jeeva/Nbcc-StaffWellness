ec2cad3d4cb2d79701f8924413de2e11c1ec6654
Kevin Jeeva <kevinjeev@gmail.com>
2021-01-21 17:38:35 -0400

    modifed some errors

--- functions/logout.php
+++ functions/logout.php
@@ -1,10 +1,6 @@
 <?php
 session_start();
-<<<<<<< HEAD
-unset($_SESSION["user"]);
-=======
 unset($_SESSION["staff_id"]);
->>>>>>> 48474e60d9bd94751050ad52ecd0476f632e2d9f
 session_destroy();
 header("Location:../login.php");
 ?>
