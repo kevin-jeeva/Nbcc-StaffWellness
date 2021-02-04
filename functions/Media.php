7480eda2df93208099c1482f1f72665ce22b0f2a
Odessa Hartjes <odessahartjes@gmail.com>
2021-02-04 12:12:23 -0400

    Added Media class

---
+++ functions/Media.php
@@ -0,0 +1,46 @@
+<?php
+include_once 'connect.php';
+
+class Media {
+    private $mediaId;
+    private $mediaDescription;
+    private $mediaPath; 
+
+    function __get($name) {
+        return $this->$name;
+    }
+    
+    function __set($name, $value) {
+        $this->$name = $value;
+    }
+
+    function __construct($mediaId,$mediaDescription,$mediaPath)
+    {
+        $this->mediaId=$mediaId;
+        $this->mediaDescription=$mediaDescription;
+        $this->mediaPath=$mediaPath;
+    }
+    
+    //select media based on media ID
+    static function getMediaById($mediaId){
+        $con = $GLOBALS['con'];
+        $sql = "SELECT media_id, media_desc, media_path from Media where media_id = '$mediaId' ";
+        echo $sql;
+        $result = mysqli_query($con, $sql);
+            while($row = mysqli_fetch_assoc($result)){
+                echo $row['media_desc']."<BR>".
+                $row['media_path'];
+            }
+    }
+    //select all media
+    static function getAllMedia(){
+        $con = $GLOBALS['con'];
+        $sql = "SELECT * from media";
+        $result = mysqli_query($con, $sql);
+            while($row = mysqli_fetch_assoc($result)){
+                echo $row['media_desc']."<BR>".
+                $row['media_path'];
+            }
+    }
+
+} // END MEDIA CLASS
