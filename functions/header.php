1f404ad968bc0e2110cebfd791c28d6feba064de
Kevin Jeeva <kevinjeev@gmail.com>
2021-02-10 14:19:14 -0400

    First change

--- functions/header.php
+++ functions/header.php
@@ -40,7 +40,7 @@ echo "<!-- Navigation -->
 				    <a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"navbarDropdown\" role=\"button\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">Exercises</a>
 				    <div class=\"dropdown-menu\" aria-labelledby=\"navbarDropdown\">
 					<a class=\"dropdown-item\" href=\"exercises_video.php\">Video Exercises</a>
-						<a class=\"dropdown-item\" href=\"exercises_sound.php\">Sound Exercises</a>
+						<a class=\"dropdown-item\" href=\"sound_exercise.php\">Sound Exercises</a>
 				    </div>
 			  	</li> 		
 			  	<li class=\"nav-item\"><a class=\"nav-link\" href=\"support.php\">Support</a></li>
