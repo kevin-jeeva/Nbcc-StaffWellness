068d7b7e3b7bf1b42c1c2c3fd93ce9da8f1f6218
Aude Doussot <doussot14@gmail.com>
2021-02-10 11:53:12 -0400

    nothing changed, git making me commit

--- functions/connect.php
+++ functions/connect.php
@@ -1,15 +1,15 @@
 <?php
 //these are defined as constants
-//define('DB_HOST', 'staff-wellness-app.c97g5kpuab3n.us-east-1.rds.amazonaws.com:3311');
-//define('DB_USER', 'admin');
-//define('DB_PASS', 'StaffNbcc001');
-//define('DB_NAME', 'staff');
+define('DB_HOST', 'staff-wellness-app.c97g5kpuab3n.us-east-1.rds.amazonaws.com:3311');
+define('DB_USER', 'admin');
+define('DB_PASS', 'StaffNbcc001');
+define('DB_NAME', 'staff');
 
 // for testing purposes
- define('DB_HOST', 'localhost:3306');
- define('DB_USER', 'root');
- define('DB_PASS', '');
- define('DB_NAME', 'staff');
+//git  define('DB_HOST', 'localhost:3306');
+// define('DB_USER', 'root');
+// define('DB_PASS', '');
+// define('DB_NAME', 'staff');
 	
 global $con;
 $con = mysqli_connect(DB_HOST,DB_USER,DB_PASS, DB_NAME);
