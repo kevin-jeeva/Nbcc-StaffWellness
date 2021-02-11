b0108dd49ea8b6dae5894cf6de8f315170775980
Odessa Hartjes <odessahartjes@gmail.com>
2021-02-11 11:09:29 -0400

    Cleaned code for update password by creating a method in Staff class

--- functions/staff.php
+++ functions/staff.php
@@ -237,5 +237,37 @@ class staff {
         }
         return false;
     }
+
+    public static function changePassword($sessId, $curPass, $newPass, $verifyNewPass){
+        $con = $GLOBALS["con"];
+       
+        $sql ="SELECT password from user WHERE staff_id = '$sessId'";
+        $result = mysqli_query($con,$sql); 
+        $row = mysqli_fetch_assoc($result);    
+        $PASS = $row["password"];
+            //if DB pass is equal to curr pass entered 
+        if(password_verify($curPass,$PASS)){    
+                //if user wrote new pass correct both times
+            if($newPass == $verifyNewPass){
+                    //update
+                $sql2 = "UPDATE user set password = '$newPass' WHERE staff_id = '$sessId' "; 
+                $result2= mysqli_query($con, $sql2);
+                echo "password updated";
+                $_SESSION["message"] = "Password Updated!";    
+              header("location:dashboard.php");
+            } else{
+                echo "passwords do not match";
+                $_SESSION["message"] = "Passwords do not match!";       
+             header("location:password_edit.php");
+            }
+        }else{
+            echo "you are not entering the correct password";
+            $_SESSION["message"] = "Password is not correct";    
+            header("location:password_edit.php");
+           // echo "<BR>". $PASS;
+            //echo "<BR>". $curPass;
+        }    
+    
+    } //end changePassword
     
 }
--- password_edit_proc.php
+++ password_edit_proc.php
@@ -5,41 +5,13 @@ include_once("functions/staff.php");
 
 $sessId = $_SESSION["staff_id"];
 $con =$GLOBALS["con"];
-
-if(isset($_POST["submit"])){
-        //grab user entered information
-    $curPass = $_POST["currentPassword"];
+$curPass = $_POST["currentPassword"];
     $newPass = $_POST["newPassword"];
     $verifyNewPass = $_POST["verifyPassword"];
-        //pull pass from database
-    $sql ="SELECT password from user WHERE staff_id = '$sessId'";
-    $result = mysqli_query($con,$sql); 
-    $row = mysqli_fetch_assoc($result);    
-    $PASS = $row["password"];
-        //if DB pass is equal to curr pass entered 
-    if(password_verify($curPass,$PASS)){    
-            //if user wrote new pass correct both times
-        if($newPass == $verifyNewPass){
-                //update
-            $sql2 = "UPDATE user set password = '$newPass' WHERE staff_id = '$sessId' "; 
-            $result2= mysqli_query($con, $sql2);
-            echo "password updated";
-            $_SESSION["message"] = "Password Updated!";    
-          header("location:dashboard.php");
-        } else{
-            echo "passwords do not match";
-            $_SESSION["message"] = "Passwords do not match!";       
-         header("location:password_edit.php");
-        }
-    }else{
-        echo "you are not entering the correct password";
-        $_SESSION["message"] = "Password is not correct";    
-        header("location:password_edit.php");
-       // echo "<BR>". $PASS;
-        //echo "<BR>". $curPass;
-    }    
-}//end isset 
-else{
-    echo "ERROR";
+
+if(isset($_POST["submit"])){
+        //call changePassword method
+    staff::changePassword($sessId,$curPass, $newPass, $verifyNewPass);
 }
+
 ?>
