78f87344cb77d44d039ab5879365219456a516fa
Kevin Jeeva <kevinjeeva1541@gmail.com>
2021-04-11 21:47:04 -0300

    added the error page in the htaccess file

---
+++ error_page.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<title>404 Error</title>
+
+	<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
+	<script src="includes/bootstrap-4.5.3-dist/jquery/jquery-3.5.1.slim.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+	<!-- Bootstrap core CSS -->
+	<script src="includes/bootstrap-4.5.3-dist/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="includes/bootstrap-4.5.3-dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+
+	<!-- Custom CSS and JS -->
+	<link rel="stylesheet" type="text/css" href="includes/styles.css">
+	<script src="functions/main.js"></script>
+</head>
+
+<body>
+
+	<!-- Navigation -->
+	<?php include('functions/header.php'); ?>
+
+	
+    <center>
+    <h2> This is a Error Page OMG :) </h2>
+    </center>
+	<!-- Quick Links -->
+	<?php include('functions/quick-links.php'); ?>
+
+	<!-- Footer -->
+	<?php include('functions/footer.php'); ?>
+
+</body>
+
+</html>
